998eda604649bab36d3b96b502d09bfd964450a1
Tuomas Rajala <tuomas.a.rajala@gmail.com>
2020-10-25 22:37:12 +0200

    Lab 7 done.

--- labs/07 JavaScript Language/js/tasks.js
+++ labs/07 JavaScript Language/js/tasks.js
@@ -4,43 +4,40 @@
 // Need a global variable:
 var tasks = []; 
 
-// Function called when the form is submitted.
+function updatePage() {
+    // Update the page:
+    var message = '';
+    var output = document.getElementById('output');
+    message = '<h2>To-Do</h2><ol>';
+    for (var i = 0, count = tasks.length; i < count; i++) {
+        message += '<li>' + tasks[i] + '</li>';
+    }
+    message += '</ol>';
+    output.innerHTML = message;
+}
+
 // Function adds a task to the global array.
 function addTask() {
     'use strict';
-
-    // Get the task:
     var task = document.getElementById('task');
-
-    // Reference to where the output goes:
-    var output = document.getElementById('output');
-    
-    // For the output:
-    var message = '';
-
     if (task.value) {
-    
-        // Add the item to the array:
         tasks.push(task.value);
-        
-        // Update the page:
-        message = '<h2>To-Do</h2><ol>';
-        for (var i = 0, count = tasks.length; i < count; i++) {
-            message += '<li>' + tasks[i] + '</li>';
-        }
-        message += '</ol>';
-        output.innerHTML = message;
-        
-    } // End of task.value IF.
-
+        updatePage();
+    }
     // Return false to prevent submission:
     return false;
-    
 } // End of addTask() function.
+
+function removeDuplicates(){
+    'use strict';
+    tasks = [...new Set(tasks)];
+    updatePage();
+}
 
 // Initial setup:
 function init() {
     'use strict';
     document.getElementById('theForm').onsubmit = addTask;
+    document.getElementById('duplicate').onclick = removeDuplicates;
 } // End of init() function.
 window.onload = init;
