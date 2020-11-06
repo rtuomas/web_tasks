8a9b6578918ecca1e126b48ba51264e33199c6a7
Tuomas Rajala <tuomas.a.rajala@gmail.com>
2020-11-06 12:44:27 +0200

    node initial commit.

---
+++ node/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+const server = app.listen(8081, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port)
+});
